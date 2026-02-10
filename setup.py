8b27eb056564ebca16e1f3338795adfdd84ac468
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2026-02-10 14:56:22 +0100

    setup fix

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup, find_packages
 setup(
     name='nearest_neighbors_loss_function',
     version='0.1.0',
-    install_requires=["skfp"],
+    install_requires=["scikit-fingerprints"],
     packages=find_packages(include=['nearest_neighbors_loss_function', 'nearest_neighbors_loss_function.*'])
 )
