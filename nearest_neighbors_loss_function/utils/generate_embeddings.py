52fd56544c2aed05ab16e4f602335f15f007521f
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2026-02-12 13:18:22 +0100

    shuffling mechanism on_off

--- nearest_neighbors_loss_function/utils/generate_embeddings.py
+++ nearest_neighbors_loss_function/utils/generate_embeddings.py
@@ -3,17 +3,24 @@ import torch
 def generate_embeddings(model, data_loader, embedding_length):
 
     model.eval()
+    original_shuffle = data_loader.batch_sampler.shuffle
+    if original_shuffle == True:
+        data_loader.batch_sampler.shuffle = False
+
     embeddings = torch.zeros([data_loader.dataset.data.x.shape[0], embedding_length], dtype=float)
     labels = torch.zeros([data_loader.dataset.data.x.shape[0], 1], dtype=int)
 
     offset = 0
     for _, data in enumerate(data_loader):
-        print(f"Batch id: {_}")
+
         batch_embeddings = model(data.x.float(), data.edge_index, data.batch)
         n_embeddings = batch_embeddings.shape[0]
         embeddings[offset: offset + n_embeddings] = batch_embeddings
         labels[offset: offset + n_embeddings] = data.y
         offset += 1
+
+    if original_shuffle == True:
+        data_loader.batch_sampler.shuffle = True
 
     embeddings = embeddings.detach().numpy()
     labels = labels.detach().numpy()
