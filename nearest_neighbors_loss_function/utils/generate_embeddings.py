c38841c572c5bfb9467297c4fe4c58114f63951a
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2026-02-12 17:24:35 +0100

    embeddings exact fit

--- nearest_neighbors_loss_function/utils/generate_embeddings.py
+++ nearest_neighbors_loss_function/utils/generate_embeddings.py
@@ -11,15 +11,16 @@ def generate_embeddings(model, data_loader, embedding_length, batch_size):
     if original_shuffle == True:
         data_loader.batch_sampler.shuffle = False
 
-    embeddings = torch.zeros([data_loader.dataset.data.x.shape[0], embedding_length], dtype=float)
-    labels = torch.zeros([data_loader.dataset.data.x.shape[0], 1], dtype=int)
+    n_samples = data_loader.dataset.data.x.shape[0]
+    embeddings = torch.zeros([n_samples, embedding_length], dtype=float)
+    labels = torch.zeros([n_samples, 1], dtype=int)
 
     offset = 0
     for _, data in enumerate(data_loader):
 
         batch_embeddings = model(data.x.float(), data.edge_index, data.batch)
-        embeddings[offset * batch_size: (offset + 1) * batch_size] = batch_embeddings
-        labels[offset * batch_size: (offset + 1) * batch_size] = data.y
+        embeddings[offset * batch_size: min((offset + 1) * batch_size, n_samples)] = batch_embeddings
+        labels[offset * batch_size: min((offset + 1) * batch_size, n_samples)] = data.y
         offset += 1
 
     if original_shuffle == True:
