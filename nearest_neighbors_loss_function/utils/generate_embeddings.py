2f2c95f8f38944568b69ea4a5d6d859ed84272ac
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2026-02-13 22:52:42 +0100

    cleanup

--- nearest_neighbors_loss_function/utils/generate_embeddings.py
+++ nearest_neighbors_loss_function/utils/generate_embeddings.py
@@ -14,13 +14,11 @@ def generate_embeddings(model, data_loader, embedding_length, batch_size):
     n_samples = data_loader.dataset.data.y.shape[0]
     embeddings = torch.zeros([n_samples, embedding_length], dtype=float)
     labels = torch.zeros([n_samples, 1], dtype=int)
-    print(f"labels.shape: {labels.shape}")
 
     start_id = 0
     for _, data in enumerate(data_loader):
         
         n_samples_batch = data.y.shape[0]
-        print(f"n_samples_batch: {n_samples_batch}")
         batch_embeddings = model(data.x.float(), data.edge_index, data.batch)
         embeddings[start_id: start_id + n_samples_batch] = batch_embeddings
         labels[start_id: start_id + n_samples_batch] = data.y
