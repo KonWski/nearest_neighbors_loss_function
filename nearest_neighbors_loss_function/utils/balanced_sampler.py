4874fc8bed922b7c8279cdbf8f563f80cb15c042
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2026-02-13 15:16:14 +0100

    sampler ids fix

--- nearest_neighbors_loss_function/utils/balanced_sampler.py
+++ nearest_neighbors_loss_function/utils/balanced_sampler.py
@@ -2,13 +2,16 @@ from torch.utils.data import BatchSampler
 import random
 
 class BalancedSampler(BatchSampler):
-    def __init__(self, labels, minority_class, minority_per_batch, 
-                 batch_size, shuffle=True, seed = 123):
+    def __init__(self, labels, n_minority_samples, minority_class, minority_per_batch, 
+                 batch_size, n_batches, shuffle=True, seed = 123):
         self.labels = labels
+        self.n_minority_samples = n_minority_samples
         self.minority_class = minority_class
         self.minority_per_batch = minority_per_batch
-        self.batch_size = batch_size
+        self.n_extra_minority_batches = self.n_minority_samples - (self.n_batches * minority_per_batch)
         self.majority_per_batch = batch_size - minority_per_batch
+        self.batch_size = batch_size
+        self.n_batches = n_batches
         self.shuffle = shuffle
         self.seed = seed
 
@@ -23,31 +26,37 @@ class BalancedSampler(BatchSampler):
     def __iter__(self):
 
         if self.shuffle:
+            random.seed(self.seed)
             random.shuffle(self.minority_indices)
             random.shuffle(self.majority_indices)
+            self.extra_minority_batch_ids = set(random.sample([i for i in range(self.n_batches)], self.n_extra_minority_batches))
 
         min_ptr, maj_ptr = 0, 0
+        batch_id = 0
 
         while (
             min_ptr + self.minority_per_batch <= len(self.minority_indices)
             and maj_ptr + self.majority_per_batch <= len(self.majority_indices)
         ):
+            extra_minority_iter = 0
+            if batch_id in self.extra_minority_batch_ids:
+                extra_minority_iter = 1
+
             batch = (
                 self.minority_indices[
-                    min_ptr : min_ptr + self.minority_per_batch
+                    min_ptr : min_ptr + self.minority_per_batch + extra_minority_iter
                 ]
                 + self.majority_indices[
                     maj_ptr : maj_ptr + self.majority_per_batch
                 ]
             )
-
+            
+            batch_id += 1
             yield batch
 
-            min_ptr += self.minority_per_batch
+            min_ptr += self.minority_per_batch + extra_minority_iter
             maj_ptr += self.majority_per_batch
 
+
     def __len__(self):
-        return min(
-            len(self.minority_indices) // self.minority_per_batch,
-            len(self.majority_indices) // self.majority_per_batch,
-        )
+        return self.n_batches
