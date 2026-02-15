baa9c41bd4c11b930746fee0dbceab77fe315a88
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2026-02-15 15:25:30 +0100

    gammacalculator refresh knn

--- nearest_neighbors_loss_function/utils/gamma_calculator.py
+++ nearest_neighbors_loss_function/utils/gamma_calculator.py
@@ -17,23 +17,10 @@ class GammaCalculator():
     def recalculate_gamma_values(self, model, data_loader, n_samples, batch_id):
         
         if self.recalculation_strategy == 0 and batch_id == 0:
-            X, y = generate_embeddings(model, data_loader, n_samples, self.embedding_length, self.device)
-            y = y.ravel()
-            proba_thrash_threshold = 1 / self.n_neighbors
-            gamma_values = torch.ones(n_samples)
+            self._refresh_knn(model, data_loader, n_samples)
 
-            knn = KNeighborsClassifier(n_neighbors=self.n_neighbors, n_jobs=-1)
-            knn.fit(X, y)
-
-            for sample_id, sample_label in enumerate(y):
-                if sample_label == 1:
-                    sample_embedding = X[sample_id, :].reshape(1, -1)
-                    sample_proba = knn.predict_proba(sample_embedding)[0][1] - proba_thrash_threshold
-                    gamma = 2 - sample_proba
-                    gamma_values[sample_id] = gamma
-
-            self.gamma_values = gamma_values
-            self.n_samples = n_samples
+        elif self.recalculation_strategy > 0 and batch_id % self.recalculation_strategy == 0:
+            self._refresh_knn(model, data_loader, n_samples)
 
         elif self.recalculation_strategy == -1:
             self.gamma_values = torch.ones(n_samples)
@@ -41,5 +28,27 @@ class GammaCalculator():
 
         self.gamma_values = self.gamma_values.to(self.device)
 
+    def _refresh_knn(self, model, data_loader, n_samples):
+
+        X, y = generate_embeddings(model, data_loader, n_samples, self.embedding_length, self.device)
+        y = y.ravel()
+        proba_thrash_threshold = 1 / self.n_neighbors
+        gamma_values = torch.ones(n_samples)
+
+        knn = KNeighborsClassifier(n_neighbors=self.n_neighbors, n_jobs=-1)
+        knn.fit(X, y)
+
+        for sample_id, sample_label in enumerate(y):
+            if sample_label == 1:
+                sample_embedding = X[sample_id, :].reshape(1, -1)
+                sample_proba = knn.predict_proba(sample_embedding)[0][1] - proba_thrash_threshold
+                gamma = 2 - sample_proba
+                gamma_values[sample_id] = gamma
+
+        self.gamma_values = gamma_values
+        self.n_samples = n_samples
+
+
     def get_gamma_values(self, gamma_start_id, gamma_end_id):
         return self.gamma_values[gamma_start_id: gamma_end_id]
+    
