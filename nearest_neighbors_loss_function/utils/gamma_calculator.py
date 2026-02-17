e27502af5a70e53fd662b1bef093e386697ac2e1
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2026-02-17 11:11:28 +0100

    focal loss

--- nearest_neighbors_loss_function/utils/gamma_calculator.py
+++ nearest_neighbors_loss_function/utils/gamma_calculator.py
@@ -1,15 +1,18 @@
 from .generate_embeddings import generate_embeddings
 from sklearn.neighbors import KNeighborsClassifier
 import torch
+import math
 
 class GammaCalculator():
 
-    def __init__(self, embedding_length, n_neighbors, batch_size, device, recalculation_strategy = 0):
+    def __init__(self, embedding_length, n_neighbors, batch_size, device, gamma_function, focal_pow = 1, recalculation_strategy = 0):
         self.embedding_length = embedding_length
         self.n_neighbors = n_neighbors
         self.batch_size = batch_size
         self.device = device
         self.gamma_values = None
+        self.gamma_function = gamma_function
+        self.focal_pow = focal_pow
         self.n_samples = None
         self.recalculation_strategy = recalculation_strategy
 
@@ -28,6 +31,7 @@ class GammaCalculator():
 
         self.gamma_values = self.gamma_values.to(self.device)
 
+
     def _refresh_knn(self, model, data_loader, n_samples):
 
         X, y = generate_embeddings(model, data_loader, n_samples, self.embedding_length, self.device)
@@ -42,7 +46,7 @@ class GammaCalculator():
             if sample_label == 1:
                 sample_embedding = X[sample_id, :].reshape(1, -1)
                 sample_proba = knn.predict_proba(sample_embedding)[0][1] - proba_thrash_threshold
-                gamma = 2 - sample_proba
+                gamma = self._calculate_gamma(sample_proba)
                 gamma_values[sample_id] = gamma
 
         self.gamma_values = gamma_values
@@ -51,4 +55,23 @@ class GammaCalculator():
 
     def get_gamma_values(self, gamma_start_id, gamma_end_id):
         return self.gamma_values[gamma_start_id: gamma_end_id]
+
+
+    def _calculate_gamma(self, sample_proba):
+
+        if self.gamma_function == "boosted_gamma":
+            return self._boosted_gamma(sample_proba)
+        elif self.gamma_function == "focal_gamma":
+            return self._focal_gamma(sample_proba)
+        else:
+            raise Exception(f"Gamma function {self.gamma_function} not implemented")
+
+
+    def _focal_gamma(self, sample_proba):
+        gamma = - math.pow(1 - sample_proba, self.focal_pow) * math.log(sample_proba)
+        return gamma
     
+
+    def _boosted_gamma(self, sample_proba):
+        gamma = 2 - sample_proba
+        return gamma
