0d77fa3986cda09811db140baa68340c5271ac8a
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2026-03-26 14:03:19 +0100

    debug

--- nearest_neighbors_loss_function/utils/batch_shaper.py
+++ nearest_neighbors_loss_function/utils/batch_shaper.py
@@ -80,6 +80,12 @@ class BatchShaper():
 
                 positive_mfs[anchor_iter] = anchor_mfs[pos_idx]
                 negative_mfs[anchor_iter] = anchor_mfs[neg_idx]
+
+                print(f"positive_mfs_distances.shape: {positive_mfs_distances.shape}")
+                print(f"distances_pos.shape: {distances_pos.shape}")
+                print(f"pos_idx: {pos_idx}")
+                print(f"neg_idx: {neg_idx}")
+
                 positive_mfs_distances[anchor_iter] = distances_pos[pos_idx]
                 negative_mfs_distances[anchor_iter] = distances_pos[neg_idx]
 
