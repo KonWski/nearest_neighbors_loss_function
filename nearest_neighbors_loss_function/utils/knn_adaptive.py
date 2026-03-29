6738a8aa793be5a85c3d33dfd261f802ee7898b8
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2026-03-29 20:15:44 +0200

    KNeigborsAdaptiveClassifier fix

--- nearest_neighbors_loss_function/utils/knn_adaptive.py
+++ nearest_neighbors_loss_function/utils/knn_adaptive.py
@@ -1,58 +1,49 @@
 import torch
-import numpy as np
 
 class KNeigborsAdaptiveClassifier():
 
-        def __init__(self, initial_n_neighbors, density_function):
-            self.initial_n_neighbors = initial_n_neighbors
-            self.density_function = density_function
-            self.probas = None
+  def __init__(self, initial_n_neighbors, density_function):
+      self.initial_n_neighbors = initial_n_neighbors
+      self.density_function = density_function
+      self.probas = None
 
-        def fit(self, distances, labels):
+  def fit(self, distances, labels):
 
-            n_neigbors_per_row = self._get_n_neigbors_per_row(distances)
+      n_neigbors_per_row = self._get_n_neigbors_per_row(distances)
 
-            print("Got n neighbors per row")
-            max_n_neigbors = int(n_neigbors_per_row.max().item())
-            print(f"max_n_neigbors: {max_n_neigbors}")
-            _, idx = torch.topk(distances, k=max_n_neigbors, dim=1, largest=False)
-            print("Got torch.topk")
+      max_n_neigbors = int(n_neigbors_per_row.max().item())
+      _, idx = torch.topk(distances, k=max_n_neigbors, dim=1, largest=False)
 
-            mask = torch.arange(max_n_neigbors)[None, :] < n_neigbors_per_row[:, None]
-            row_ids = torch.arange(distances.size(0))[:, None].expand_as(idx)
+      mask = torch.arange(max_n_neigbors)[None, :] < n_neigbors_per_row[:, None]
+      row_ids = torch.arange(distances.size(0))[:, None].expand_as(idx)
 
-            selected_rows = row_ids[mask]
-            selected_cols = idx[mask]
+      selected_rows = row_ids[mask]
+      selected_labels = labels[selected_rows]
 
-            selected_y = labels[selected_rows, selected_cols]
+      row_sums = torch.zeros(distances.size(0), dtype=selected_rows.dtype)
+      row_sums.scatter_add_(0, selected_rows, selected_labels)
 
-            row_sums = torch.zeros(distances.size(0), dtype=torch.float)
-            row_sums.scatter_add_(0, selected_rows, selected_y.float())
-            print("scatter_add_")
+      self.probas = row_sums / n_neigbors_per_row
 
-            probas = row_sums / n_neigbors_per_row
+  def _get_n_neigbors_per_row(self, distances):
 
-            self.probas = probas
+      density = self._local_reachability_distance(distances)
+      avg_density = torch.mean(density)
+      n_neigbors_per_row = torch.floor(self.initial_n_neighbors * (avg_density / density))
+      return n_neigbors_per_row
 
-        def _get_n_neigbors_per_row(self, distances):
+  def _local_reachability_distance(self, distances):
 
-            density = self._local_reachability_distance(distances)
-            avg_density = torch.mean(density)
-            n_neigbors_per_row = torch.floor(self.initial_n_neighbors * (avg_density / density))
-            return n_neigbors_per_row
+      indices = torch.argsort(distances, dim=1)[:, 1:self.initial_n_neighbors+1] # skip self
+      distances = torch.gather(distances, 1, indices)
 
-        def _local_reachability_distance(self, distances):
+      k_dist = torch.sort(distances, dim=1).values[:, self.initial_n_neighbors-1]
 
-            indices = torch.argsort(distances, dim=1)[:, 1:self.initial_n_neighbors+1] # skip self
-            distances = torch.gather(distances, 1, indices)
+      k_dist_neighbors = k_dist[indices]
+      reach_dist = torch.maximum(distances, k_dist_neighbors)
 
-            k_dist = torch.sort(distances, dim=1).values[:, self.initial_n_neighbors-1]
+      lrd = 1.0 / (torch.mean(reach_dist, dim=1) + 1e-10)
+      return lrd
 
-            k_dist_neighbors = k_dist[indices]
-            reach_dist = torch.maximum(distances, k_dist_neighbors)
-
-            lrd = 1.0 / (torch.mean(reach_dist, dim=1) + 1e-10)
-            return lrd
-
-        def predict_proba(self, id):
-            return self.probas[id]
+  def predict_proba(self, id):
+      return self.probas[id]
