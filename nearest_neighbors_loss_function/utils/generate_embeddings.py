5ede1d50ff867d33e20a930e06c19898759aa2a8
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2026-04-04 00:11:09 +0200

    debug

--- nearest_neighbors_loss_function/utils/generate_embeddings.py
+++ nearest_neighbors_loss_function/utils/generate_embeddings.py
@@ -5,12 +5,15 @@ def generate_embeddings(model, data_loader, n_samples, embedding_length, device)
 
     model.eval()
     try:
-       original_shuffle = data_loader.batch_sampler.shuffle
+        original_shuffle = data_loader.batch_sampler.shuffle
     except AttributeError: 
         original_shuffle = False
 
     if original_shuffle == True:
         data_loader.batch_sampler.shuffle = False
+
+    print(f"data_loader.batch_sampler.shuffle: {data_loader.batch_sampler.shuffle}")
+    print(f"original_shuffle: {original_shuffle}")
 
     embeddings = torch.zeros([n_samples, embedding_length], dtype=float)
     labels = torch.zeros([n_samples, 1], dtype=int)
