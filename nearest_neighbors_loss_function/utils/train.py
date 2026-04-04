3f9203afdb7404758dbfd814a9b20e2db2b4f8fa
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2026-04-05 00:21:28 +0200

    debug

--- nearest_neighbors_loss_function/utils/train.py
+++ nearest_neighbors_loss_function/utils/train.py
@@ -132,6 +132,8 @@ def train(model, train_loader, n_train_samples, optimizer, loss_function, batch_
 
             loss = loss_function(anchor_mf, positive_mf, negative_mf)
             gamma_values = gamma_calculator.get_gamma_values(gamma_start_id, gamma_end_id, positive_mf_distances, negative_mf_distances)
+            print(f"labels: {labels}")
+            print(f"gamma_values: {gamma_values}")
             loss = loss * gamma_values
             loss = loss.mean()
 
