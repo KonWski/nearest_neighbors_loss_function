411be663a95fa84a3c5d3b404d60bc6a25ecefee
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2026-04-06 01:28:03 +0200

    debug

--- nearest_neighbors_loss_function/utils/train.py
+++ nearest_neighbors_loss_function/utils/train.py
@@ -128,16 +128,12 @@ def train(model, train_loader, n_train_samples, optimizer, loss_function, batch_
             data = convert_graph_data_to_float(data)
             anchor_mfs = model(data)
             anchor_mf, positive_mf, positive_mf_distances, negative_mf, negative_mf_distances, _ = batch_shaper.shape_batch(anchor_mfs, labels)
-            print(f"Shaper labels: {_}")
 
             loss = loss_function(anchor_mf, positive_mf, negative_mf)
-            print(f"Loss before: {loss}")
             gamma_values = gamma_calculator.get_gamma_values(gamma_start_id, gamma_end_id, positive_mf_distances, negative_mf_distances)
-            print(f"gamma_values: {gamma_values}")
+            print(f"gamma_values.requires_grad: {gamma_values.requires_grad}")
             loss = loss * gamma_values
-            print(f"Loss after: {loss}")
             loss = loss.mean()
-            print(f"Loss mean: {loss}")
 
             running_loss += loss.item()
             loss.backward()
