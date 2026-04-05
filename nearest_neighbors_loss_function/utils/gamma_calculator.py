3fa186c2b51ad82b8608011ff53d6a3df2b287cc
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2026-04-06 00:19:10 +0200

    weight test

--- nearest_neighbors_loss_function/utils/gamma_calculator.py
+++ nearest_neighbors_loss_function/utils/gamma_calculator.py
@@ -87,7 +87,7 @@ class GammaCalculator():
             # distances_1 = distances[mask, :]
             # proba_1 = knn.predict_proba(distances_1)[0][1] - proba_thrash_threshold
             # gamma_1 = self._calculate_gamma(proba_1)
-            # self.gamma_values = torch.ones(n_samples)
+            self.gamma_values = torch.ones(n_samples)
             # self.gamma_values[mask] = gamma_1
             self.gamma_values[mask] = 2
 
