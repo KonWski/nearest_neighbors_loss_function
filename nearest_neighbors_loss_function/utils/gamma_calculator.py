4f3f6f515f8f95887bf12a4e10d11595adcfbc9b
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2026-04-06 15:08:18 +0200

    yet another try

--- nearest_neighbors_loss_function/utils/gamma_calculator.py
+++ nearest_neighbors_loss_function/utils/gamma_calculator.py
@@ -61,15 +61,16 @@ class GammaCalculator():
         y = y.ravel()
         proba_thrash_threshold = 1 / self.n_neighbors
         self.n_samples = n_samples
-        mask = (y == 1)
+        mask_1 = (y == 1)
+        mask_0 = (y == 0)
 
         if self.density_awareness:
             knn = KNeigborsAdaptiveClassifier(self.n_neighbors, self.density_function)
             knn.fit(distances, y)
-            proba_1 = knn.predict_proba(mask)
+            proba_1 = knn.predict_proba(mask_1)
             gamma_1 = self._calculate_gamma(proba_1)
             self.gamma_values = torch.ones(n_samples, dtype=gamma_1.dtype)
-            self.gamma_values[mask] = gamma_1
+            self.gamma_values[mask_1] = gamma_1
 
         else:
 
@@ -84,11 +85,18 @@ class GammaCalculator():
             y = y.numpy()
 
             knn.fit(distances, y)
-            distances_1 = distances[mask, :]
+
+            distances_0 = distances[mask_0, :]
+            proba_0 = knn.predict_proba(distances_0)[0][0] - proba_thrash_threshold
+            gamma_0 = self._calculate_gamma(proba_0)
+
+            distances_1 = distances[mask_1, :]
             proba_1 = knn.predict_proba(distances_1)[0][1] - proba_thrash_threshold
             gamma_1 = self._calculate_gamma(proba_1)
+
             self.gamma_values = torch.ones(n_samples)
-            self.gamma_values[mask] = gamma_1
+            self.gamma_values[mask_0] = gamma_0
+            self.gamma_values[mask_1] = gamma_1
 
 
     def get_gamma_values(self, gamma_start_id, gamma_end_id, positive_mf_distances, negative_mf_distances):
