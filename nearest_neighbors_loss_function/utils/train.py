72ef419ea22e53c9f16834efc1c9a8f798112d86
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2026-04-09 17:44:47 +0200

    triplet margin loss, smaller margin

--- nearest_neighbors_loss_function/utils/train.py
+++ nearest_neighbors_loss_function/utils/train.py
@@ -54,7 +54,7 @@ def train_triplet(
     # iterate over all given seeds
     for id_seed, seed in enumerate(seeds):
 
-        loss_function = TripletMarginLoss(reduction="none")
+        loss_function = TripletMarginLoss(reduction="none", margin=0.5)
         batch_shaper = BatchShaper(device, training_type)
         gamma_calculator = GammaCalculator(embedding_length, n_neighbors, batch_size, device, gamma_function, focal_pow, 
                                            gamma_recalculation_strategy, weight_distances, density_awareness, density_function,
