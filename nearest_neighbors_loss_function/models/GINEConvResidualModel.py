3a8e522939650c48571370e07df9d1ed1c6334b9
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2026-04-16 16:22:03 +0200

    import fix

--- nearest_neighbors_loss_function/models/GINEConvResidualModel.py
+++ nearest_neighbors_loss_function/models/GINEConvResidualModel.py
@@ -1,5 +1,5 @@
-from torch.nn import ModuleList, Sequential, Linear, ReLU, Module, BatchNorm1d, Dropout
-from torch_geometric.nn import global_mean_pool, init
+from torch.nn import ModuleList, Sequential, Linear, ReLU, Module, BatchNorm1d, Dropout, init
+from torch_geometric.nn import global_mean_pool
 from torch_geometric.nn.conv import GINEConv
 
 class GINEConvResidualModel(Module):
