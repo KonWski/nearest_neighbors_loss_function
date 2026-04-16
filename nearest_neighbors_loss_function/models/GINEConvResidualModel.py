63055816e490b1d6e54dc463ce0f6bb2ab1f3bdd
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2026-04-17 00:21:24 +0200

    edge_dim fix

--- nearest_neighbors_loss_function/models/GINEConvResidualModel.py
+++ nearest_neighbors_loss_function/models/GINEConvResidualModel.py
@@ -69,7 +69,7 @@ class GINEBlock(Module):
             ReLU(),
             Linear(hidden_dim, hidden_dim)
         )
-        self.gine_conv = GINEConv(self.mlp, edge_dim=3)
+        self.gine_conv = GINEConv(self.mlp, edge_dim=hidden_dim)
         self.batch_norm = BatchNorm1d(hidden_dim)
         self.relu = ReLU()
         self.dropout = Dropout(p=0.2)
