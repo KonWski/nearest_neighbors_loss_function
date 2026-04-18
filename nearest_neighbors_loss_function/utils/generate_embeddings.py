dc1c06a3a456c2652fe927f458d3182979d3d13a
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2026-04-18 17:44:31 +0200

    memory fix

--- nearest_neighbors_loss_function/utils/generate_embeddings.py
+++ nearest_neighbors_loss_function/utils/generate_embeddings.py
@@ -16,19 +16,17 @@ def generate_embeddings(model, data_loader, n_samples, embedding_length, device)
     labels = torch.zeros([n_samples, 1], dtype=int)
 
     start_id = 0
-    with torch.no_grad():
+    for _, data in enumerate(data_loader):
 
-        for _, data in enumerate(data_loader):
+        data = data.to(device)
+        data = convert_graph_data_to_float(data)
+        n_samples_batch = data.y.shape[0]
+        batch_embeddings = model(data)
+        embeddings[start_id: start_id + n_samples_batch] = batch_embeddings
+        labels[start_id: start_id + n_samples_batch] = data.y
+        start_id += n_samples_batch
 
-            data = data.to(device)
-            data = convert_graph_data_to_float(data)
-            n_samples_batch = data.y.shape[0]
-            batch_embeddings = model(data)
-            embeddings[start_id: start_id + n_samples_batch] = batch_embeddings.detach().cpu()
-            labels[start_id: start_id + n_samples_batch] = data.y
-            start_id += n_samples_batch
-
-            del data, batch_embeddings
+        del data, batch_embeddings
 
     if original_shuffle == True:
         data_loader.batch_sampler.shuffle = True
