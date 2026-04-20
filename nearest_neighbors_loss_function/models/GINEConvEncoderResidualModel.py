6cb88913f08d24d9f32c9d8cbab66385eee50ece
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2026-04-20 14:07:22 +0200

    debug

--- nearest_neighbors_loss_function/models/GINEConvEncoderResidualModel.py
+++ nearest_neighbors_loss_function/models/GINEConvEncoderResidualModel.py
@@ -36,7 +36,11 @@ class GINEConvEncoderResidualModel(Module):
             data.x, data.edge_index, data.edge_attr, data.batch
         )
 
-        x = self.node_encoder(x)
+        try:
+            x = self.node_encoder(x)
+        except:
+            print(f"x.dtype: {x.dtype}")
+            print(x)
         edge_attr = self.edge_encoder(edge_attr)
 
         for layer_block in self.layer_blocks:
