5c6e8d1e36f41095b14196211fefe57de1304779
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2026-06-25 20:07:23 +0200

    adaptive neighborhood fix

--- nearest_neighbors_loss_function/utils/knn_adaptive.py
+++ nearest_neighbors_loss_function/utils/knn_adaptive.py
@@ -5,61 +5,66 @@ class KNeigborsAdaptiveClassifier():
     def __init__(self, initial_n_neighbors, density_function):
         self.initial_n_neighbors = initial_n_neighbors
         self.density_function = density_function
-        self.probas = None
+        self.pred_probas = None
 
     def fit(self, distances, labels):
 
         n_neigbors_per_row = self._get_n_neigbors_per_row(distances)
+        
+        # fill diagonal with inf to ommit these elements
+        distances_diag_inf = distances.masked_fill(
+            torch.eye(distances.size(0), dtype=torch.bool, device=distances.device),
+            float('inf')
+        )
 
-        max_n_neigbors = int(n_neigbors_per_row.max().item())
-        _, idx = torch.topk(distances, k=max_n_neigbors, dim=1, largest=False)
+        # indices of k nearest neighbors
+        _, idx = torch.topk(distances_diag_inf, k=self.initial_n_neighbors, dim=1, largest=False)
 
-        mask = torch.arange(max_n_neigbors)[None, :] < n_neigbors_per_row[:, None]
-        row_ids = torch.arange(distances.size(0))[:, None].expand_as(idx)
+        # labels of k nearest neighbors        
+        labels_k_nn = labels[idx]
 
-        selected_rows = row_ids[mask]
-        selected_labels = labels[selected_rows]
+        # mask to select specific number of neighbors per row
+        mask = torch.arange(self.initial_n_neighbors)[None, :] < n_neigbors_per_row[:, None]
+        
+        self.pred_probas = (labels_k_nn * mask).sum(dim=1) / n_neigbors_per_row
 
-        row_sums = torch.zeros(distances.size(0), dtype=selected_rows.dtype)
-        row_sums.scatter_add_(0, selected_rows, selected_labels)
-
-        self.probas = row_sums / n_neigbors_per_row
 
     def _get_n_neigbors_per_row(self, distances):
 
-        density = self._local_reachability_distance(distances)
-        density = torch.log(density + 1e-10)
-        avg_density = torch.mean(density)
-
-        # make sure that avg_density / density does not explode
-        n_neigbors_per_row_computed = torch.floor(self.initial_n_neighbors * (avg_density / density))
+        local_density = self._local_reachability_density(distances)
+        avg_density = torch.mean(local_density)
+        
+        n_neigbors_per_row_computed = torch.floor(self.initial_n_neighbors * (avg_density / local_density))
         n_neigbors_per_row_min = torch.full_like(n_neigbors_per_row_computed, self.initial_n_neighbors)
         n_neigbors_per_row = torch.minimum(n_neigbors_per_row_computed, n_neigbors_per_row_min)
 
         return n_neigbors_per_row
 
-    def _local_reachability_distance(self, distances):
+    def _local_reachability_density(self, distances):
+        '''
+        local reachability distance calculated according to the LOF method
+        
+        lrd = n_neighbors / sum_reachability_distances
+        '''
 
-        indices = torch.argsort(distances, dim=1)[:, 1:self.initial_n_neighbors+1] # skip self
-        distances = torch.gather(distances, 1, indices)
+        # distances to n nearest neighbors
+        indices_nn = torch.argsort(distances, dim=1)[:, 1:self.initial_n_neighbors+1] # skip self
+        distances_nn = torch.gather(distances, 1, indices_nn)
 
-        k_dist = torch.sort(distances, dim=1).values[:, self.initial_n_neighbors-1]
+        # distances to the most distance neighbor
+        k_dist = torch.sort(distances_nn, dim=1).values[:, self.initial_n_neighbors-1]
 
-        k_dist_neighbors = k_dist[indices]
-        reach_dist = torch.maximum(distances, k_dist_neighbors)
+        # k_dist for all of the k neighbors
+        k_dist_neighbors = k_dist[indices_nn]
 
-        lrd = 1.0 / (torch.mean(reach_dist, dim=1) + 1e-10)
+        # reachability distances necessary to calculate the local density
+        reach_dist = torch.maximum(distances_nn, k_dist_neighbors)
+        
+        # local_reachability_density
+        lrd = self.initial_n_neighbors / (torch.sum(reach_dist, dim=1) + 1e-10)
+        
         return lrd
-
-    def _density(self, distances):
-
-        indices = torch.argsort(distances, dim=1)[:, 1:self.initial_n_neighbors+1]
-        knn_distances = torch.gather(distances, 1, indices)
-
-        mean_dist = torch.mean(knn_distances, dim=1)
-        density = mean_dist + 1e-10
-
-        return density
+    
 
     def predict_proba(self, id):
-        return self.probas[id]
+        return self.pred_probas[id]
