ee75e18dc1977a81dffc82743454e6f3725994d4
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2026-07-27 16:58:53 +0200

    debug train_params

--- nearest_neighbors_loss_function/utils/workflows.py
+++ nearest_neighbors_loss_function/utils/workflows.py
@@ -28,6 +28,8 @@ def train_workflow(args):
     group_experiment_hash = uuid4().hex
     group_experiment_save_path = create_hash_dir(args.save_path, group_experiment_hash)
 
+    print(f"train_params: {train_params}")
+
     for train_params in train_param_holder:
 
         statistics, best_seed_models, n_train_samples = train_triplet(
