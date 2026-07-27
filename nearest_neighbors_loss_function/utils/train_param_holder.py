13c8cf53999ceb816c26fd7ac85b30a4953c37dc
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2026-07-27 17:07:49 +0200

    debug train_params

--- nearest_neighbors_loss_function/utils/train_param_holder.py
+++ nearest_neighbors_loss_function/utils/train_param_holder.py
@@ -29,6 +29,7 @@ class TrainParamHolder:
         training_params = TrainModelParams().get_keys()
         starting_combination = {param: arguments for param, arguments in self.args.items() 
                                 if param not in list_params and param in training_params}
+        print(f"starting_combination: {starting_combination}")
 
         for args_combination in list_param_combinations:
             combination = TrainModelParams()
